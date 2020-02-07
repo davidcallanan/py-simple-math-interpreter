b2205c9ca07d08be32df1fc348669783eb131d77
David Callanan <822995-dcal@users.noreply.gitlab.com>
2020-02-07 16:53:42 +0000

    update lexer tests

--- lexer_test.py
+++ lexer_test.py
@@ -7,22 +7,20 @@ class TestLexer(unittest.TestCase):
 	def test_empty(self):
 		tokens = list(Lexer("").generate_tokens())
 		self.assertEqual(tokens, [])
+	
+	def test_whitespace(self):
+		tokens = list(Lexer(" \t\n  \t\t\n\n").generate_tokens())
+		self.assertEqual(tokens, [])
 
 	def test_numbers(self):
-		tokens = list(Lexer("123").generate_tokens())
-		self.assertEqual(tokens, [Token(TokenType.NUMBER, 123.000)])
-
-		tokens = list(Lexer("123.456").generate_tokens())
-		self.assertEqual(tokens, [Token(TokenType.NUMBER, 123.456)])
-
-		tokens = list(Lexer("123.").generate_tokens())
-		self.assertEqual(tokens, [Token(TokenType.NUMBER, 123.000)])
-		
-		tokens = list(Lexer(".456").generate_tokens())
-		self.assertEqual(tokens, [Token(TokenType.NUMBER, 000.456)])
-
-		tokens = list(Lexer(".").generate_tokens())
-		self.assertEqual(tokens, [Token(TokenType.NUMBER, 000.000)])
+		tokens = list(Lexer("123 123.456 123. .456 .").generate_tokens())
+		self.assertEqual(tokens, [
+			Token(TokenType.NUMBER, 123.000),
+			Token(TokenType.NUMBER, 123.456),
+			Token(TokenType.NUMBER, 123.000),
+			Token(TokenType.NUMBER, 000.456),
+			Token(TokenType.NUMBER, 000.000),
+		])
 	
 	def test_operators(self):
 		tokens = list(Lexer("+-*/").generate_tokens())
@@ -30,19 +28,15 @@ class TestLexer(unittest.TestCase):
 			Token(TokenType.PLUS),
 			Token(TokenType.MINUS),
 			Token(TokenType.MULTIPLY),
-			Token(TokenType.DIVIDE)
+			Token(TokenType.DIVIDE),
 		])
 
 	def test_parens(self):
 		tokens = list(Lexer("()").generate_tokens())
 		self.assertEqual(tokens, [
 			Token(TokenType.LPAREN),
-			Token(TokenType.RPAREN)
+			Token(TokenType.RPAREN),
 		])
-
-	def test_whitespace(self):
-		tokens = list(Lexer(" \t\n  \t\t\n\n").generate_tokens())
-		self.assertEqual(tokens, [])
 
 	def test_illegal_characters(self):
 		with self.assertRaises(Exception):
@@ -61,5 +55,5 @@ class TestLexer(unittest.TestCase):
 			Token(TokenType.NUMBER, 48),
 			Token(TokenType.RPAREN),
 			Token(TokenType.MULTIPLY),
-			Token(TokenType.NUMBER, 51)
+			Token(TokenType.NUMBER, 51),
 		])
